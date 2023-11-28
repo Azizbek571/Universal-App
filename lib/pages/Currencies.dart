195f54f2bfc78c4825bf2d7c00ec4b8fe28529bd
Azizbek <azizbek.mukhtorov.m@mail.ru>
2023-11-29 02:33:11 +0300

    done: Prayer Time

--- lib/pages/Currencies.dart
+++ lib/pages/Currencies.dart
@@ -21,25 +21,28 @@ class _currenciesState extends State<currencies> {
           body: SingleChildScrollView(
               child: Container(
             padding: EdgeInsetsDirectional.symmetric(horizontal: 16),
-            child:
-            controller.loading ? const Center(child: CircularProgressIndicator()):
-             Column(
-                children: List.generate(controller.currencies.length, (index) {
-              var item = controller.currencies[index];
-              return ListTile(
-                title: Text.rich(TextSpan(
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                    children: [
-                      TextSpan(text: item['Nominal'] + " "),
-                      TextSpan(text: item['CcyNm_UZ']),
-                    ])),
-                subtitle: Text(item['Date']),
-                trailing: Text(
-                  item['Rate'],
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-                ),
-              );
-            })),
+            child: controller.loading
+                ? const Center(child: CircularProgressIndicator())
+                : Column(
+                    children:
+                        List.generate(controller.currencies.length, (index) {
+                    var item = controller.currencies[index];
+                    return ListTile(
+                      title: Text.rich(TextSpan(
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                          children: [
+                            TextSpan(text: item['Nominal'] + " "),
+                            TextSpan(text: item['CcyNm_UZ']),
+                          ])),
+                      subtitle: Text(item['Date']),
+                      trailing: Text(
+                        item['Rate'],
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w700),
+                      ),
+                    );
+                  })),
           )),
         );
       },
--- lib/pages/PrayerTime.dart
+++ lib/pages/PrayerTime.dart
@@ -9,19 +9,79 @@ class PrayerTime extends StatefulWidget {
 }
 
 class _PrayerTimeState extends State<PrayerTime> {
-  HomeController controller=Get.put(HomeController());
+  HomeController controller = Get.put(HomeController());
+  List times = [];
+  filterTime(data) {
+    data.forEach((key, value) {
+      times.add({"time": key, "hour": value});
+    });
+  }
+
+  @override
+  void initState() {
+    filterTime(controller.prayerTime['times']);
+    super.initState();
+  }
+
+  String filterText(String key) {
+    switch (key) {
+      case "tong_saharlik":
+        return "Bomdod namozi";
+      case "quyosh":
+        return "Quyosh";
+      case "peshin":
+        return "Peshin namozi";
+      case "asr":
+        return "Asr namozi";
+      case "shom_iftor":
+        return "Shom namozi";
+      case "hufton":
+        return "Xufton namozi";
+
+      default:
+        return "";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${controller.selectCity} vaqti bilan"),
-      ),
-      body: Column(
-        children:[
-          Text(controller.prayerTime.toString())
-        ],
-      )
+        appBar: AppBar(
+          title: Text("${controller.selectCity} vaqti bilan"),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                Text(controller.prayerTime['date'], style: TextStyle(fontSize: 20, fontWeight:FontWeight.w600),),
+                Text(controller.prayerTime['weekday'], style: TextStyle(fontSize: 20, fontWeight:FontWeight.w600),),
+              ],
 
-    );
+              ),
+              const Divider(),
+              Column(
+                children: List.generate(times.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 2),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(filterText(times[index]['time']),
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600)),
+                        Text(times[index]['hour'],
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600)),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ],
+          ),
+        ));
   }
 }
